afa69b304507c72fd84080e174aff38aa9479965
Neel Smith <dnsmith.neel@gmail.com>
2019-01-30 12:42:40 -0500

    Test first two required functions

--- src/test/scala/DiplomaticOrthographySpec.scala
+++ src/test/scala/DiplomaticOrthographySpec.scala
@@ -1,5 +1,5 @@
 package edu.holycross.shot.ocre
-
+import edu.holycross.shot.mid.validator._
 import org.scalatest.FlatSpec
 
 
@@ -11,4 +11,12 @@ class DiplomaticOrthographySpec extends FlatSpec {
     val expected = "Diplomatic orthography of OCRE coin legends"
     assert(DiplomaticLegendOrthography.orthography == expected)
   }
+
+  it should "determine if a code point is valid" in pending
+  it should "identify accepted token categories"  in {
+    val expected = Set(LexicalToken, PunctuationToken, MarkupToken)
+    assert (DiplomaticLegendOrthography.tokenCategories.toSet == expected)
+  }
+  it should "parse a citable node into a sequence of tokens" in pending
+
 }
--- src/test/scala/NormalizedOrthographySpec.scala
+++ src/test/scala/NormalizedOrthographySpec.scala
@@ -1,5 +1,5 @@
 package edu.holycross.shot.ocre
-
+import edu.holycross.shot.mid.validator._
 import org.scalatest.FlatSpec
 
 
@@ -11,4 +11,11 @@ class NormalizedOrthographySpec extends FlatSpec {
     val expected = "Orthography for normalized and expanded edition of OCRE coin legends"
     assert(NormalizedLegendOrthography.orthography == expected)
   }
+
+  it should "determine if a code point is valid" in pending
+  it should "identify accepted token categories"  in {
+    val expected = Set(LexicalToken, PunctuationToken, MarkupToken)
+    assert (NormalizedLegendOrthography.tokenCategories.toSet == expected)
+  }
+  it should "parse a citable node into a sequence of tokens" in pending
 }
